5317951a72275bb278e9f0d1d1d89fa59806f7a0
ysfyavuzz <206433915+ysfyavuzz@users.noreply.github.com>
2025-11-27 08:55:12 +0300

    fix: Remove orphaned template files - Deleted app/modal.tsx (Expo template, unused) - Deleted src/components/ThemedView.tsx (import wrapper for deleted files) These files were importing from deleted /components directory

--- src/components/ThemedView.tsx
+++
@@ -1,8 +0,0 @@
-// Re-export ThemedView from root components
-export { ThemedView } from '../../components/themed-view';
-
-// Helper function for backwards compatibility
-export const createView = () => {
-  const { ThemedView } = require('../../components/themed-view');
-  return ThemedView;
-};
